77dd1958f86ccfe50ed356c4c342a0530f658c90
Sayed Aman Konen <sayed.konen@comviva.com>
2025-09-03 00:24:54 +0530

    SwiftUI day 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "Ted"
+name = "Rebbeca"
+name = "Kee;ey"
+
+let character = "Daphne"
+
+var playerName = "Megan"
+print(playerName)
+playerName = "Alex"
+print(playerName)
+playerName = "Jordan"
+print(playerName)
+playerName = "Taylor"
+print(playerName)
+
