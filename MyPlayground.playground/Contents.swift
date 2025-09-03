a403e9b627b1d7119342806e457c6d91ca1cc0d6
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-03 23:10:58 +0530

    Added str int

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -17,3 +17,18 @@ playerName = "Taylor"
 print(playerName)
 print(playerName)
 
+// STRING
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+let nameLength = quote.count
+print(nameLength)
+print(movie.uppercased())
+print(movie.hasPrefix("A day"))
+// INT
+let reallyBig = 100_000_000
+let number = 120
+print(number.isMultiple(of: 3))
