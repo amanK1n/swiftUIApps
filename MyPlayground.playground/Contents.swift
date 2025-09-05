349c150e8b7747eaae1cbb3f76e8998817b9340c
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-05 22:26:36 +0530

    Added Day-1 code

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -32,3 +32,22 @@ print(movie.hasPrefix("A day"))
 let reallyBig = 100_000_000
 let number = 120
 print(number.isMultiple(of: 3))
+// Double
+let num = 0.1 + 0.2
+print(num)
+print(round(num))
+// Bool
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+let number1 = 120
+print(number1.isMultiple(of: 3))
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+var gameOver = false
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
