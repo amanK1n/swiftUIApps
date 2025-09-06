5975ef228a64e8e29ce59b7729ebb1b210a13dfa
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-06 21:47:27 +0530

    Added str code

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -53,3 +53,22 @@ gameOver.toggle()
 print(gameOver)
 
 print("5 x 5 is \(5 * 5)")
+var numsss = "3223"
+numsss = numsss.replacingOccurrences(of: "3", with: "")
+print("sss::", numsss)
+var numx = Array(numsss)
+print(numx)
+print(numx.count)
+var nnn = numx.compactMap { Int(String($0)) }
+print(nnn)
+
+/////
+print("#########$$$$$$$$$NEW LINE######")
+var nums = [7,25,21,2,20,7,24,9,24,24,6,22,5,1,26,17,18,29,25,9,8,27,6,26,8,5,27,5,0,29,26,29,24,18,23,14,25,17,15,20,11,22,4,17,15,0,26,3,21,21,12,0,10,10,26,19,15,23,16,7,14,12,7,8,0,0,14,26,18,22,8,21,6,12,0,21,4,26,16,26,18,21]
+
+for i in nums.enumerated() {
+    if let index = nums.firstIndex(of: 26) {
+        nums.remove(at: index)
+    }
+}
+print(nums)
