e68086f0ee0a2ca0c213ea0fbcf7cb7df02f47c7
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-11 23:50:56 +0530

    Find largest elem

--- StriverArray.playground/Contents.swift
+++ StriverArray.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 import Foundation
 print("Striver Array Video")
+// Largest Element in an Array..
+let arr = [3,2,1,4,5,-8,70,54,-2,12]
+var largest = Int.min
+
+for i in arr {
+    if i > largest {
+        largest = i
+    }
+}
+
+print(largest)
