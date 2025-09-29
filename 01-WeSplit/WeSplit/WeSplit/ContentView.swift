d4ea01389eda9759275df3b063afca0e0759f395
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-30 00:05:09 +0530

    Added WeSplit app

--- 01-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ 01-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -6,8 +6,31 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                   Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+               }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+/// Below is for TEST
+struct TestContentView: View {
     @State private var tapCount = 0
     var body: some View {
         NavigationStack {
@@ -74,7 +97,4 @@ struct LoopView: View {
             }
         }
     }
-}
-#Preview {
-    ContentView()
 }
