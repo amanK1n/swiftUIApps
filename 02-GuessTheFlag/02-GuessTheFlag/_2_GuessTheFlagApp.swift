a67176ca9099f430df2187703d9e5a5ef8d2f57a
Sayed Aman Konen <amankonen0799@gmail.com>
2025-09-30 22:44:26 +0530

    Added Guess flag initial file

---
+++ 02-GuessTheFlag/02-GuessTheFlag/_2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_GuessTheFlagApp.swift
+//  02-GuessTheFlag
+//
+//  Created by Sayed on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct _2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
