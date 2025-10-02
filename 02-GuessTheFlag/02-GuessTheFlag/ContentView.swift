5a75403ca138d0f55905756a7be5fbbe651548c4
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-02 22:34:42 +0530

    Added final code

--- 02-GuessTheFlag/02-GuessTheFlag/ContentView.swift
+++ 02-GuessTheFlag/02-GuessTheFlag/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    
     var body: some View {
-        Text("kkk")
+        ZStack {
+            LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of..")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                ForEach(0..<3) { item in
+                    Button {
+                        flagTapped(item)
+                    } label: {
+                        Image(countries[item])
+                            .clipShape(.buttonBorder)
+                            .shadow(radius: 10)
+                    }
+                }
+            }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+        .ignoresSafeArea()
+        
+        
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// Below codes are for syntax only
 struct CustomAlertView: View {
     @State private var showingAlert = false
     var body: some View {
