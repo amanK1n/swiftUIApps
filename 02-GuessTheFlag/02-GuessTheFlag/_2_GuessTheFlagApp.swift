91eab18d5a6d817bf6315bdfaef896f96643cf0e
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-02 20:59:12 +0530

    Added alert code

--- 02-GuessTheFlag/02-GuessTheFlag/_2_GuessTheFlagApp.swift
+++ 02-GuessTheFlag/02-GuessTheFlag/_2_GuessTheFlagApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct _2_GuessTheFlagApp: App {
     var body: some Scene {
         WindowGroup {
-            ButtonView()
+            CustomAlertView()
         }
         
         
