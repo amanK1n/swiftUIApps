ca6582c069d6a2742c6642c96a3992b5dbef3dcc
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-02 23:28:24 +0530

    Added new design

--- 02-GuessTheFlag/02-GuessTheFlag/ContentView.swift
+++ 02-GuessTheFlag/02-GuessTheFlag/ContentView.swift
@@ -16,16 +16,23 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.purple, .mint], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-            
-            VStack(spacing: 30) {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+            VStack(spacing: 15) {
                 VStack {
                     Text("Tap the flag of..")
-                        .foregroundStyle(.white)
+                        .foregroundStyle(.secondary)
                         .font(.subheadline.weight(.heavy))
                     
                     Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
                         .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { item in
@@ -37,7 +44,17 @@ struct ContentView: View {
                             .shadow(radius: 10)
                     }
                 }
-            }
+            }.frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }.padding()
         }.alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
