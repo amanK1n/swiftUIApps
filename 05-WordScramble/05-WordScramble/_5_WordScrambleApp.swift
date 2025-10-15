039e89425730834525f37079ad1e09690467b3a4
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-15 07:00:58 +0530

    Added init file of word scramble

---
+++ 05-WordScramble/05-WordScramble/_5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_WordScrambleApp.swift
+//  05-WordScramble
+//
+//  Created by Sayed on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
