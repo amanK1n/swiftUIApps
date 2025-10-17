1758bceeeef1f714c67c0ae18140cef66067d288
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-17 19:33:07 +0530

    Added list code

--- 05-WordScramble/05-WordScramble/ContentView.swift
+++ 05-WordScramble/05-WordScramble/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    var people = ["AMAN", "NIKHAT"]
     var body: some View {
         List {
             Section("Aman") {
@@ -19,12 +20,47 @@ struct ContentView: View {
                     Text("Dynamic Text \($0)")
                 }
             }
+            Section("Fur itch") {
+                
+                ForEach(people, id: \.self) {
+                    Text($0)
+                }
+            }
         }.listStyle(.grouped)
     }
 }
 
 
-
+struct ListViewExample: View {
+    var people = ["AMAN", "NIKHAT"]
+    var body: some View {
+        List {
+            Section("Aman") {
+                Text("Sayed")
+                Text("Sayed")
+            }
+            Section("NIKHAT") {
+                ForEach(0..<5) {
+                    Text("Dynamic Text \($0)")
+                }
+            }
+            Section("Fur itch") {
+                
+                ForEach(people, id: \.self) {
+                    Text($0)
+                }
+            }
+        }.listStyle(.grouped)
+    }
+    
+    func testBundles() {
+        if let fileURL = Bundle.main.url(forResource: "nameOfYourFile", withExtension: "txt") {
+            if let fileContent = try? String(contentsOf: fileURL) {
+                // We load ed file as string
+            }
+        }
+    }
+}
 
 #Preview {
     ContentView()
