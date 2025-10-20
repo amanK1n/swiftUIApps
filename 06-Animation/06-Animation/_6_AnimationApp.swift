8d59cd4da52cf1e6bb980c03f62b39dbf96530b0
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-20 21:51:11 +0530

    Added animation code

---
+++ 06-Animation/06-Animation/_6_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_AnimationApp.swift
+//  06-Animation
+//
+//  Created by Sayed on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _6_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
