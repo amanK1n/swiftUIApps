82a5a62e2c37091be81eedb1096f49aa6cfdb828
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-26 23:04:45 +0530

    Added codable code

--- 07-iExpense/07-iExpense/ContentView.swift
+++ 07-iExpense/07-iExpense/ContentView.swift
@@ -7,9 +7,28 @@
 
 import SwiftUI
 import Observation
+
 struct ContentView: View {
     var body: some View {
         Text("")
+    }
+}
+
+struct User2: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+struct CodeableDemo: View {
+    @State private var user = User2(firstName: "Taylor", lastName: "Swift")
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
