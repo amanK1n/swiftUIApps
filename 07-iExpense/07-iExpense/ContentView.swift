7f16a1bea838a8da964f2445a195a92803b131fc
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-28 11:07:22 +0530

    Added code

--- 07-iExpense/07-iExpense/ContentView.swift
+++ 07-iExpense/07-iExpense/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 import Observation
 
+struct ExpenseItem: Identifiable {
+    var id = UUID()
+    
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @State private var expenses: Expenses = Expenses()
     var body: some View {
-        Text("")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }.onDelete(perform: removeItem)
+            }.navigationTitle("iExpense")
+            
+             .toolbar {
+                    Button("Add expense", systemImage: "plus") {
+                        let expense = ExpenseItem(name: "Text expence", type: "Personal", amount: 5)
+                        expenses.items.append(expense)
+                    }
+             }
+             
+        }
+    }
+    func removeItem(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
