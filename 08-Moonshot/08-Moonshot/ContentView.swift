623f8a6768ba5f860da681e8977b7dbf6a19d181
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-29 08:02:37 +0530

    Added new proj 08

---
+++ 08-Moonshot/08-Moonshot/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  08-Moonshot
+//
+//  Created by Sayed on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct CodableDataExample: View {
+    var body: some View {
+        Button("Deconde JSUN") {
+            let ip = """
+            {
+                "name": "Yailor Wift",
+                "address": {
+                    "street": "555, Wailor Qwift Venyu",
+                    "city": "Nashville"
+                }
+            }
+            """
+            let data = Data(ip.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print("Name: ", user.name)
+                print("Street: ", user.address.street)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ 08-Moonshot/08-Moonshot/_8_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_MoonshotApp.swift
+//  08-Moonshot
+//
+//  Created by Sayed on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _8_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CodableDataExample()
+        }
+    }
+}
