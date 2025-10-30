ca8a1646282a096aab3c9eba445d24566f4224d9
Sayed Aman Konen <amankonen0799@gmail.com>
2025-10-30 22:39:56 +0530

    Added grid vue

--- 08-Moonshot/08-Moonshot/ContentView.swift
+++ 08-Moonshot/08-Moonshot/ContentView.swift
@@ -14,6 +14,22 @@ struct ContentView: View {
 }
 
 
+struct AdaptiveGridExample: View {
+    let layout = [GridItem(.adaptive(minimum: 80))]
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Text \($0)")
+                }
+            }
+        }
+    }
+}
+
+
+
+
 struct User: Codable {
     let name: String
     let address: Address
