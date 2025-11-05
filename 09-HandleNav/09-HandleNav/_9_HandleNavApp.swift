6a35a7786c92e07625b0c37d5a43d908f7097fc2
Sayed Aman Konen <amankonen0799@gmail.com>
2025-11-05 08:36:00 +0530

    Added handle NAV

---
+++ 09-HandleNav/09-HandleNav/_9_HandleNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_HandleNavApp.swift
+//  09-HandleNav
+//
+//  Created by Sayed on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _9_HandleNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
