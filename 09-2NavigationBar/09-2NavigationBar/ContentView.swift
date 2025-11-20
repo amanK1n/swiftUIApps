ca1646f0eee014d9a1f360e9062052f32c31a314
Sayed Aman Konen <amankonen0799@gmail.com>
2025-11-20 13:53:30 +0530

    Added nav bar code

--- 09-2NavigationBar/09-2NavigationBar/ContentView.swift
+++ 09-2NavigationBar/09-2NavigationBar/ContentView.swift
@@ -6,8 +6,45 @@
 //
 
 import SwiftUI
-
+import Foundation
 struct ContentView: View {
+    var body: some View {
+        Example2()
+    }
+}
+struct Example2: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello Aman!!")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) { // Toolbar Item is placed for placment of btn ONLY
+                        Button("Tappp meh!") {
+                            print("Button tapped !")
+
+                        }
+                        
+                    }
+                    
+                    
+                    ToolbarItemGroup(placement: .topBarLeading) {
+                        Button("Btn 1") {
+                            print("Btn1 tapped!")
+                        }
+                        
+                        Button("Btn 2") {
+                            print("Btn2 tapped!")
+                        }
+                    }
+                }
+        }
+    }
+}
+
+
+
+
+// Navigatin bar customization - BG color, Dark Scheme, hidden
+struct Example1: View {
     var body: some View {
         NavigationStack {
             List(0..<100) { i in
@@ -21,6 +58,8 @@ struct ContentView: View {
         }
     }
 }
+
+
 
 #Preview {
     ContentView()
