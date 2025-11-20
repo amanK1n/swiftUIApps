231dc0fbded30e78fd0ecbc0efdb91fb6c48a409
Sayed Aman Konen <amankonen0799@gmail.com>
2025-11-20 13:26:38 +0530

    Added nav code

---
+++ 09-2NavigationBar/09-2NavigationBar/_9_2NavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_2NavigationBarApp.swift
+//  09-2NavigationBar
+//
+//  Created by Sayed on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _9_2NavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
