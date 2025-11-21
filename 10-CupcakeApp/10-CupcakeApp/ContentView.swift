099970a30af28aa33317cbcd192b9eaa8a1b8deb
Sayed Aman Konen <amankonen0799@gmail.com>
2025-11-21 23:39:13 +0530

    Add code

---
+++ 10-CupcakeApp/10-CupcakeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  10-CupcakeApp
+//
+//  Created by Sayed on 20/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, cup cakee!")
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
