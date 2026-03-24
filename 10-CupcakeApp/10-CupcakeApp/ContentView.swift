a0967f7d2689264b9f5b26e10f386d92e75f4615
Sayed Aman Konen <amankonen0799@gmail.com>
2026-03-24 12:26:32 +0530

    Added code

--- 10-CupcakeApp/10-CupcakeApp/ContentView.swift
+++ 10-CupcakeApp/10-CupcakeApp/ContentView.swift
@@ -6,25 +6,33 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, cup cakee!")
-            
-            
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, cup cakee!")
+//            
+//            
+//        }
+//        .padding()
+        
+        ZStack {
+            Color.black.ignoresSafeArea()
+                       
+                     
         }
-        .padding()
         
     }
 }
 
-
-
-
 #Preview {
     ContentView()
 }
+
+
+
+
