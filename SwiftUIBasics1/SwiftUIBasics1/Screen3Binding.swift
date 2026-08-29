a0ddabd4a5d64de58619e38b505cb99a6f8479f0
Sayed Aman Konen <sayed.konen@comviva.com>
2026-08-29 22:08:10 +0530

    Added code

--- SwiftUIBasics1/SwiftUIBasics1/Screen3Binding.swift
+++ SwiftUIBasics1/SwiftUIBasics1/Screen3Binding.swift
@@ -7,24 +7,41 @@
 
 import Foundation
 import SwiftUI
+public import Combine
 
 struct Screen3: View {
-    @State private var isOn: Bool = false
+    @State private var isOn: Bool = false// EX-1
+    @StateObject public var user: User = User() // EX-2
     var body: some View {
         VStack {
+            //EX-1 This is binding example
             Text("Screen 3 Binding")
                 .foregroundStyle(isOn ? .red : Color.secondary)
                 .font(.headline)
                 .padding()
-           
             Screen3ChildViewToggle(isOn: $isOn)
-           
+            //EX-1 Binding ENDS HERE
+            //*****######********#######******
+            
+           //EX-2 START ObservedObject, Observable Object, Published
+            Text("Your name is: \(user.name)")
+            TextField("Enter your name", text: $user.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button("Tap", action: {
+                debugPrint(user.name)
+            })
+            // EX-2 END
+            
             
         }
     }
 }
 
 
+// For binding
+// Assume This chlid is in another file
 struct Screen3ChildViewToggle: View {
     @Binding var isOn: Bool
     var body: some View {
@@ -36,3 +53,7 @@ struct Screen3ChildViewToggle: View {
     }
 }
 
+// CLASS with Observable Object, Published
+public class User: ObservableObject {
+   @Published var name: String = String()
+}
