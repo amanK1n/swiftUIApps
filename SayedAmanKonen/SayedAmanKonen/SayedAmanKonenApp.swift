54de1c84c363414b588824d4cab1b9608f658b2b
Sayed Aman Konen <sayed.konen@comviva.com>
2026-09-01 11:52:32 +0530

    Adde api call

---
+++ SayedAmanKonen/SayedAmanKonen/SayedAmanKonenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayedAmanKonenApp.swift
+//  SayedAmanKonen
+//
+//  Created by comviva on 01/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SayedAmanKonenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
